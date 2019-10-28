05044b539e9f8501378993ff87481cd748cfbc3c
Allen Shin <geeuho@gmail.com>
2019-10-28 03:17:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,11 +11,11 @@ def load_library(yaml_file)
   response
 end
 
-def get_japanese_emoticon(yaml_file, emoticon)
+def get_japanese_emoticon(yaml_file, emote)
   library = load_library(yaml_file)
   response = nil
   library["get_emoticon"].each do |english, japanese|
-    if emoticon == english
+    if emote == english
       response = japanese
     end
   end
@@ -27,12 +27,12 @@ def get_japanese_emoticon(yaml_file, emoticon)
 end
 
 
-def get_english_meaning(yaml_file, emoticon)
-  library = load_library(yaml_file)
+def get_english_meaning(yaml, emote)
+  library = load_library(yaml)
   response = nil
-  library["get_meaning"].each do |emo, trans|
-    if emo == emoticon
-      response = trans
+  library["get_meaning"].each do |emoticon, translation|
+    if emoticon == emote
+      response = translation
     end
   end
   if response == nil
